f2edefad7329842a10cedd563fd33bf10b7f88cf
aaaujm <juniorrafael95@yahoo.com>
2021-01-20 20:26:08 -0300

    CategoryDTO

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dscatalog.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dscatalog.entities.Category;
+
+public class CategoryDTO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long id; 
+	private String name;
+	
+	
+	public CategoryDTO(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	public CategoryDTO() {
+		super();
+		
+	}
+	
+	public CategoryDTO(Category category) {
+		super();
+		this.id = category.getId();
+		this.name = category.getName();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
