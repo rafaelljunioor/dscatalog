525f9034d4c5263a1d9d5953e5386148d57a4cc2
aaaujm <juniorrafael95@yahoo.com>
2021-01-25 20:48:48 -0300

    PUT Category and ResourceNotFoundException fixed

--- backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/ResourceNotFoundException.java
@@ -2,14 +2,14 @@ package com.devsuperior.dscatalog.services.exceptions;
 
 import java.util.Arrays;
 
-public class EntityNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public EntityNotFoundException (String msg) {
+	public ResourceNotFoundException (String msg) {
 		super(msg);
 	}
 	
