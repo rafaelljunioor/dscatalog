ac268b3312827cbe678d7d84d2e017604cd1ca2d
aaaujm <juniorrafael95@yahoo.com>
2021-01-25 21:34:27 -0300

    Delete category developed and audtion data on entity developed

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,11 +1,15 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -21,6 +25,11 @@ public class Category implements Serializable{
 	private Long id;
 	private String name;
 	
+	@Column(columnDefinition="TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition="TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 	public Category() {
 		
 		
@@ -50,6 +59,26 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
+	
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createdAt= Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt= Instant.now();
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
