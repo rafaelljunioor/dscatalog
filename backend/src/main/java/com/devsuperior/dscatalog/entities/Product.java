fa5bcc0f601ada360a9aacb0ae984674f7915066
aaaujm <juniorrafael95@yahoo.com>
2021-01-26 21:00:43 -0300

    Product Repository, service, resource developed and HTTP GET method ok

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -90,6 +90,14 @@ public class Product implements Serializable{
 		}
 		
 		
+		public Instant getDate() {
+			return date;
+		}
+
+		public void setCategories(Set<Category> categories) {
+			this.categories = categories;
+		}
+
 		public Set<Category> getCategories() {
 			return categories;
 		}
